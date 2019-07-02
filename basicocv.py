5e9752041bf3a9db1df6bc70e19a9e4a326bb1d1
aryan <51536512+aryancse@users.noreply.github.com>
2019-07-02 08:51:37 +0530

    basic for starting thecamera

---
+++ basicocv.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import cv2 as cv
+
+#checking the version
+print(cv.__version__)
+
+#images read
+img=cv.imread('wp3778678.jpg',-1)
+img1=cv.imread('wp3778678.jpg',0)
+#  1 means the same color channel BGR
+#  0 means the no color channel only black and white
+# -1 means the image tranparency
+print(img)
+
+#print shape
+print(img.shape)
+
+#to display img
+cv.imshow('murat',img[0:250,0:150]) #to crop 
+cv.imshow('m2',img1)
+#to make the window open
+cv.waitKey(0)  #0 means for the infinte time or it is in mili/micro sec
+
+
