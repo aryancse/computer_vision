0a971bbf2c9f8b785882a64648a948a9eb94f633
aryan <51536512+aryancse@users.noreply.github.com>
2019-07-04 22:35:00 +0530

    Add files via upload

---
+++ deteco.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+#to detect the color (red)
+
+
+import cv2 as cv
+
+cap=cv.VideoCapture(0)
+
+#camera stauts
+while cap.isOpened():
+    status,frame=cap.read()
+    #detecting red color
+    redimg=cv.inRange(frame,(0,0,0),(90,90,255))
+
+    cv.imshow('liveredcolor',redimg)
+    if cv.waitKey(10)   &  0xff == ord('q'):
+        break
+
+cv.destroyAllWindows()
+cap.release()
